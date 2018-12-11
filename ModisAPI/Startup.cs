1e89ea83189f038866593ba02693d2fbd6c01948
KeoiMadBro <davide.donato8gmail.com>
2018-12-11 11:00:28 +0100

    commit

--- ModisAPI/Startup.cs
+++ ModisAPI/Startup.cs
@@ -27,8 +27,7 @@ namespace ModisAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddScoped<IWorkerServiceStudenti,WorkerServiceStudenti>();
-
+            services.AddScoped<IWorkerServiceStudenti, WorkerServiceStudenti>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
